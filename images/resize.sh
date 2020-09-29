f04e90e1e91c1cb24cb4c114b9e0ce16fcb7dcec
Michael Kopp <kopp.michael@yahoo.de>
2020-09-29 08:49:44 +0200

    resize images Lebensmittel

--- images/resize.sh
+++ images/resize.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 
-for small in bananen.jpg decken.png gewuerze.png huete.png kinderspielzeug.png logo.png pflegeprodukte.png schmuck.png spiele.png tasche.png wohnungseinrichtung.png nudelhaus.jpg
+
+for small in bananen.jpg decken.png gewuerze.png huete.png kinderspielzeug.png logo.png pflegeprodukte.png schmuck.png spiele.png tasche.png wohnungseinrichtung.png nudelhaus.jpg kaffee.jpg leckereien.jpg reis_mehl_qinoa.jpg saft.jpg wein.jpg
 do
     mogrify -resize 220 $small
 done

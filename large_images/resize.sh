a4b8fea54a21513259cea37ce70d225426f7cb8c
Michael Kopp <kopp.michael@yahoo.de>
2020-09-30 17:22:21 +0200

    Lebensmittel: Add PopUp images

--- large_images/resize.sh
+++ large_images/resize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-for large in decken.png huete.png kinderspielzeug.png klangeschalen.png mehr_kunsthandwerk.png pflegeprodukte.png resize.sh schmuck.png spiele.png tasche.png wohnungseinrichtung.png
+for large in decken.png huete.png kinderspielzeug.png klangeschalen.png mehr_kunsthandwerk.png pflegeprodukte.png resize.sh schmuck.png spiele.png tasche.png wohnungseinrichtung.png gewuerze.png kaffee.jpg leckereien.jpg reis_mehl_qinoa.jpg saft.jpg wein.jpg
 do
     mogrify -resize 1200 $large
 done

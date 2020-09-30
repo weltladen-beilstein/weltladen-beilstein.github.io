5b1784bc0c3fc62f677ffea0bd28adce54e2536b
Michael Kopp <kopp.michael@yahoo.de>
2020-09-30 17:34:30 +0200

    kunsthandwerk: add more images in gallery only

--- large_images/resize.sh
+++ large_images/resize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-for large in decken.png huete.png kinderspielzeug.png klangeschalen.png mehr_kunsthandwerk.png pflegeprodukte.png resize.sh schmuck.png spiele.png tasche.png wohnungseinrichtung.png gewuerze.png kaffee.jpg leckereien.jpg reis_mehl_qinoa.jpg saft.jpg wein.jpg
+for large in decken.png huete.png kinderspielzeug.png klangeschalen.png mehr_kunsthandwerk.png pflegeprodukte.png resize.sh schmuck.png spiele.png tasche.png wohnungseinrichtung.png gewuerze.png kaffee.jpg leckereien.jpg reis_mehl_qinoa.jpg saft.jpg wein.jpg armband.png geldbeutel.png rassel.png stempel.png windlicht.png
 do
     mogrify -resize 1200 $large
 done

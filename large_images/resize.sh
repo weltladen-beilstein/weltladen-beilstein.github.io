3aa7607dc8a4e345bc16d330fd400fc12a0018ae
Michael Kopp <kopp.michael@yahoo.de>
2020-12-03 21:07:09 +0100

    aktuelles: Weihnachtsprodukte

--- large_images/resize.sh
+++ large_images/resize.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 
-for large in decken.png huete.png kinderspielzeug.png klangeschalen.png mehr_kunsthandwerk.png pflegeprodukte.png resize.sh schmuck.png spiele.png tasche.png wohnungseinrichtung.png gewuerze.png kaffee.jpg leckereien.jpg reis_mehl_qinoa.jpg saft.jpg wein.jpg armband.png geldbeutel.png rassel.png stempel.png windlicht.png handtaschen.jpg
+for large in $@
 do
+    echo resizing image $large
     mogrify -resize 1200 $large
 done
 
